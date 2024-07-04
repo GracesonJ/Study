b61f5c04f8a61890dab4b30de2eec56ea5e3a3a5
Graceson J <gracesonjjohnson@gmail.com>
2024-07-04 22:44:58 +0530

    first commit

---
+++ Basics/content.js
@@ -0,0 +1,12 @@
+// To display Contend   - console.log(content)
+
+console.log("hey welcome");
+console.log("123456789");
+
+console.log("welcome:", 2025);
+//concantenation
+console.log("Welcome: " + 2024);
+
+// type of content
+console.log(typeof "hello");
+console.log(typeof 2024);
---
+++ Basics/var.js
@@ -0,0 +1,16 @@
+//variables in JS
+
+//var
+console.log("firstData = "+firstData);
+var firstData
+
+console.log("Initially, firstdata = "+firstData);
+console.log("Initially, type of firstdata = ", typeof firstData);
+
+firstData = 1000
+console.log("Now, firstdata = "+firstData);
+console.log("Now, type of firstdata = ", typeof firstData);
+
+firstData = "JS"
+console.log("After updating, firstdata = "+firstData);
+console.log("After updating, type of firstdata = ", typeof firstData);
