4577beb2b3c5bfd9d0e59ba0f4f6d36a93bc887f
Graceson J <gracesonjjohnson@gmail.com>
2024-07-04 23:06:28 +0530

    const added

---
+++ Basics/const.js
@@ -0,0 +1,10 @@
+//const variable
+
+// console.log("WeekDays="+weekDays); // (Hoisting) o/p Error
+const weekDays = 7
+console.log("Initially, WeekDays="+ weekDays); // o/p Initially, WeekDays=7
+console.log("Initally, typeof weekDays ="+ typeof weekDays); // o/p Initally, typeof weekDays = number 
+
+// weekDays = "Monday"
+// console.log("Now, WeekDays="+ weekDays); // o/p Error
+// console.log("Now, typeof weekDays ="+ typeof weekDays); // o/p Error
