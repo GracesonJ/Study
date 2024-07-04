8dec4e35002802d974b596e6ad113857d9b18854
Graceson J <gracesonjjohnson@gmail.com>
2024-07-04 23:58:20 +0530

    template literal added

---
+++ Basics/tem.js
@@ -0,0 +1,12 @@
+const company = "Grace Infotech"
+const location = "Delhi"
+const zipcode = 5289
+
+console.log("using commas");
+console.log("Company Name is", company, "and its Location is", location, "and its Zipcode is", zipcode);
+
+console.log("using concatenation");
+console.log("Company Name is"+ company +" and its Location is " + location + " and its Zipcode is " + zipcode);
+
+console.log("using Template Literal")
+console.log(`Company Name is ${company} and its Location is ${location} and its Zipcode is ${zipcode}`);
