c8e236e887934c7b0b0f4346490767174c6ed272
Graceson J <gracesonjjohnson@gmail.com>
2024-07-09 21:47:44 +0530

    operators added

--- Basics/op.js
+++ Basics/op.js
@@ -34,3 +34,19 @@ empId = false
 console.log(`!empId : ${!empId}`);
 empId = true
 console.log(`!empId : ${!empId}`);
+
+// Increment and Decrement operators
+
+r=20
+console.log(`Before applying Increment : r= ${r}`);
+// console.log(`After applying Increment : r= ${r++}`); //post increment operator
+// console.log(r);
+console.log(`After applying Increment : r= ${++r}`); //pre increment operator
+console.log(`r= ${r}`);
+
+r=20
+console.log(`Before applying Decrement : r= ${r}`);
+// console.log(`After applying Decrement : r= ${r--}`); //post Decrement operator
+// console.log(`r= ${r}`);
+console.log(`After applying Decrement : r= ${--r}`); //pre Decrement operator
+console.log(`r= ${r}`);
